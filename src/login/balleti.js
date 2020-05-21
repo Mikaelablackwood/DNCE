27c32f1592e04e00e5737bbe7ea972ddcb26ad99
Mikaelablackwood <62964642+Mikaelablackwood@users.noreply.github.com>
2020-05-21 01:49:01 -0400

    Create balleti.js

---
+++ src/login/balleti.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { StyleSheet, Text, View, Button, ScrollView, Image, } from 'react-native'
+import { Video } from 'expo-av';
+import { Permissions } from 'expo'
+
+
+export default class BalletI extends React.Component {
+
+render () {
+
+
+return (
+<View style={styles.container}>
+
+
+
+
+<iframe width="300" height="300" src="https://www.youtube.com/embed/Uy67yin1ylI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+</View>
+)
+}
+}
+
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+backgroundColor: '#fff',
+alignItems: 'center',
+justifyContent: 'center',
+marginTop: 30
+}
+})
